3668ff676309c4ee9e468855531f2de5e26227b7
italo OM <italoangeloolivaresmartinez@gmail.com>
2026-09-15 21:20:53 -0500

    registro de visitas a funcion global

---
+++ ejercicio5.py
@@ -0,0 +1,14 @@
+
+# funciones globales
+
+visitas = 0
+# global
+def registrar_visita():
+    global visitas
+    visitas += 1
+
+lista_visitas=[]
+print(f"Visita #{visitas} registrada")
+registrar_visita() # Visita #1
+registrar_visita() # Visita #2
+print(f"Total: {visitas}") # Total: 2
